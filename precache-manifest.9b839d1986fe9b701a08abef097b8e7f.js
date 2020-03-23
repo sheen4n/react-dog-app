691539c40028cad2ae38a5d68f08a8e0cb83ccb6
sheen4n <sheen.an.goh@gmail.com>
2020-03-23 11:20:31 +0800

    Updates

---
+++ precache-manifest.9b839d1986fe9b701a08abef097b8e7f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322eb9a7e1044e9479c9534969945080",
+    "url": "/react-dog-app/index.html"
+  },
+  {
+    "revision": "80524d1cdaaa8baf72b5",
+    "url": "/react-dog-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c7296d25290567503d22",
+    "url": "/react-dog-app/static/css/main.8a3c274a.chunk.css"
+  },
+  {
+    "revision": "80524d1cdaaa8baf72b5",
+    "url": "/react-dog-app/static/js/2.a58559ab.chunk.js"
+  },
+  {
+    "revision": "150e7ebf1b16402b62fb665d1a7869df",
+    "url": "/react-dog-app/static/js/2.a58559ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7296d25290567503d22",
+    "url": "/react-dog-app/static/js/main.0d9b88b5.chunk.js"
+  },
+  {
+    "revision": "e18cd10ef05eb1bd21cc",
+    "url": "/react-dog-app/static/js/runtime-main.bd6e615b.js"
+  },
+  {
+    "revision": "4eb85e80248f3371f721a9687e16d075",
+    "url": "/react-dog-app/static/media/hazel.4eb85e80.jpg"
+  },
+  {
+    "revision": "2f47cb659ae081c9e07bc25c264f0aed",
+    "url": "/react-dog-app/static/media/tubby.2f47cb65.jpg"
+  },
+  {
+    "revision": "5c1e3b17efc4e2197352567ea0e262c1",
+    "url": "/react-dog-app/static/media/whiskey.5c1e3b17.jpg"
+  }
+]);
